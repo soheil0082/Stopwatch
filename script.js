065cc9faf0d2a2ce1c1b462496620264d7386320
soheil bayani <baayani.2.82@gmail.com>
2023-07-07 10:03:02 +0330

    html/css/js parts fully complete

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let startTime;
-let container = document.querySelector(".container");
+let time = document.querySelector(".time");
+let startBtn = document.querySelector("#start");
+let stopBtn = document.querySelector("#stop");
+let resetBtn = document.querySelector("#reset");
+let timeStampObj = document.querySelector(".timeStamps");
 
-function startStopwatch() {
-  startTime = new Date();
-}
-startStopwatch();
-setInterval(function () {
-  let currentTime = new Date();
+let timer;
+let currentTime;
+let isStarted = false;
+
+function getCurrentTime() {
   let finalTime = currentTime.getTime() - startTime.getTime();
 
   let hours = Math.floor(finalTime / 1000 / 60 / 60);
@@ -17,5 +20,38 @@ setInterval(function () {
   finalTime -= seconds * 1000;
   let miliSeconds = finalTime;
 
-  container.innerHTML = `${hours} : ${minutes} : ${seconds} : ${miliSeconds}`;
-}, 1);
+  return `${String(hours).padStart(2, "0")} : ${String(minutes).padStart(
+    2,
+    "0"
+  )} : ${String(seconds).padStart(2, "0")} : ${String(miliSeconds).padStart(
+    3,
+    "0"
+  )}`;
+}
+
+function startStopwatch() {
+  if (isStarted) return;
+
+  isStarted = true;
+  startTime = new Date();
+
+  timer = setInterval(function () {
+    currentTime = new Date();
+
+    time.innerHTML = getCurrentTime();
+  }, 1);
+}
+
+function stopStopwatch() {
+  timeStampObj.innerHTML += `<div>${getCurrentTime()}</div>`;
+}
+
+function resetStopwatch() {
+  clearInterval(timer);
+  isStarted = false;
+  startStopwatch();
+}
+
+startBtn.addEventListener("click", startStopwatch);
+stopBtn.addEventListener("click", stopStopwatch);
+resetBtn.addEventListener("click", resetStopwatch);
